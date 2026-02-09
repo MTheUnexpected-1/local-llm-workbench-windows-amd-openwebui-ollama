99b6783102066642376280510fc8ba8ae2e762ae
MTheUnexpected1 <fqf21@txstate.edu>
2026-02-08 22:58:45 -0600

    test

--- installer/build-installer.ps1
+++ installer/build-installer.ps1
@@ -1,10 +1,14 @@
 $ErrorActionPreference = "Stop"
+
+# Kill running app to prevent EBUSY lock
+Get-Process "Local LLM Workbench" -ErrorAction SilentlyContinue | Stop-Process -Force
+
 Push-Location "$PSScriptRoot\..\app"
 npm install
 npm run package
 Pop-Location
 
 Push-Location $PSScriptRoot
-wix build .\burn\Bundle.wxs -o ..\dist\LocalLLMWorkbench-Setup.exe
+& "$PSScriptRoot\..\app\node_modules\.bin\wix.cmd" build .\burn\Bundle.wxs -o ..\dist\LocalLLMWorkbench-Setup.exe
 Pop-Location
 Write-Host "Created dist\\LocalLLMWorkbench-Setup.exe"
